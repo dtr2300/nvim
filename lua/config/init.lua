34b034b68903eb2e4983f5fbd7dd2dc091493cea
Dieter Hoofman <dieter.hoofman@gmail.com>
2021-09-24 16:34:33 +0200

    Cache packer_compiled.lua

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -4,4 +4,5 @@ require "config.autocmd"
 require "config.mappings".setup()
 require "config.utils.globals"
 require "config.plugins"
+require "config.plugins.packer_compiled"
 require "config.gui".setup_no_ginit()
