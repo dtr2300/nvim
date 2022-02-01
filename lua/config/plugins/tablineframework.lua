280ab3687de4ca2ae363afc04eca3c27bf1c4a64
Dieter Hoofman <dieter.hoofman@gmail.com>
2022-02-01 15:07:38 +0100

    Update

--- lua/config/plugins/tablineframework.lua
+++ lua/config/plugins/tablineframework.lua
@@ -1,34 +1,39 @@
 local M = {}
 
-local margin = 4 -- size of the left margin
-local short = false -- display buffer indices only
-local max_len = 90 -- max name length for standard layout
-local alt = false -- use alternative layout
-local alt_auto_width = true -- automatically adjust tabwidth for alt layout
-local alt_width = 10 -- tabsize for alt layout if alt_auto_width = false
-local alt_width_short = 2 -- tabsize for alt layout - short
+local opts = {
+  margin = 4, -- size of the left margin
+  short = false, -- display buffer indices only
+  max_len = 90, -- max name length for standard layout
+  alt = false, -- use alternative layout
+  alt_auto_width = true, -- automatically adjust tabwidth for alt layout
+  alt_width = 10, -- tabsize for alt layout if alt_auto_width = false
+  alt_width_short = 2, -- tabsize for alt layout - short
+}
 
 local function ls()
   return vim.tbl_filter(function(buf)
-    return vim.api.nvim_buf_is_valid(buf)
-          and vim.api.nvim_buf_get_option(buf, "buflisted")
+    return vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_get_option(buf, "buflisted")
   end, vim.api.nvim_list_bufs())
 end
 
 local function trunc(s, length)
-  return #s > length and string.sub(s, 1, length - 1) .. "…" or s
+  return string.sub(s, 1, length - 1) .. "…"
+end
+
+local function trunc_name(name)
+  return #name > opts.max_len and trunc(name, opts.max_len) or name
 end
 
 local function format_name(name)
   local auto_width = 0
-  if alt and not short and alt_auto_width then
-    local w = vim.api.nvim_get_option("columns") - margin - 3
+  if opts.alt and not opts.short and opts.alt_auto_width then
+    local w = vim.api.nvim_get_option("columns") - opts.margin - 3
     local n = #ls()
     auto_width = math.min(math.floor(w / n) - 7, math.floor(w / 2) - 7)
   end
-  local max = short and alt_width_short or (alt_auto_width and auto_width or alt_width)
+  local max = opts.short and opts.alt_width_short or (opts.alt_auto_width and auto_width or opts.alt_width)
   if #name > max then
-    name = string.sub(name, 1, max - 1) .. "…"
+    name = trunc(name, max)
   else
     name = name .. string.rep(" ", max - #name)
   end
@@ -37,24 +42,24 @@ end
 
 local function render(f)
   local c = require"onedark.colors"
-  f.add(string.rep(" ", margin))
+  f.add(string.rep(" ", opts.margin))
 
   f.make_bufs(function(info)
-    f.add(alt and { "▎ ", fg=info.current and c.red or c.grey } or " ")
+    f.add(opts.alt and { "▎ ", fg=info.current and c.red or c.grey } or " ")
 
     local name
     if info.filename then
       local icon_color = info.current and f.icon_color(info.filename) or c.light_grey
       f.add { f.icon(info.filename) .. " ", fg=icon_color }
-      name = short and tostring(info.index) or info.filename
+      name = opts.short and tostring(info.index) or info.filename
     else
       f.add(" ")
-      name = short and tostring(info.index) or "new " .. info.index
+      name = opts.short and tostring(info.index) or "new " .. info.index
     end
-    name = alt and format_name(name) or trunc(name, max_len)
+    name = opts.alt and format_name(name) or trunc_name(name)
     f.add(name)
 
-    f.add(info.modified and " ● " or (alt and "   " or " "))
+    f.add(info.modified and " ● " or (opts.alt and "   " or " "))
   end)
 
   f.add_spacer()
@@ -73,32 +78,32 @@ function M.toggle_colors()
 end
 
 function M.toggle_short()
-  short = not short
+  opts.short = not opts.short
   vim.cmd "redrawtabline"
 end
 
 function M.toggle_alt()
-  alt = not alt
+  opts.alt = not opts.alt
   vim.cmd "redrawtabline"
 end
 
 function M.toggle_auto_width()
-  alt_auto_width = not alt_auto_width
+  opts.alt_auto_width = not opts.alt_auto_width
   vim.cmd "redrawtabline"
 end
 
 function M.set_margin(width)
-  margin = width
+  opts.margin = width
   vim.cmd "redrawtabline"
 end
 
 function M.set_max_len(width)
-  max_len = width
+  opts.max_len = width
   vim.cmd "redrawtabline"
 end
 
 function M.set_width(width)
-  alt_width = width
+  opts.alt_width = width
   vim.cmd "redrawtabline"
 end
 
