b84404b039c9ab02141eeeb4819352fc511d5d61
Dieter Hoofman <dieter.hoofman@gmail.com>
2022-02-23 16:02:26 +0100

    Update

--- lua/config/utils/exec.lua
+++ lua/config/utils/exec.lua
@@ -4,7 +4,7 @@ function M.exec_selected_rows()
   local _, end_row, _ = unpack(vim.fn.getcurpos())
   local start_row = vim.fn.line "v"
   local lines = vim.api.nvim_buf_get_lines(0, start_row - 1, end_row, true)
-  loadstring(table.concat(lines, "\n"))()
+  assert(loadstring(table.concat(lines, "\n")))()
 end
 
 return M
