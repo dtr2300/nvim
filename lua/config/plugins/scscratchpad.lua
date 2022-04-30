e2f67f5ac65b6d61b1d8f6e8334377c921979eb9
Dieter Hoofman <dieter.hoofman@gmail.com>
2022-04-30 20:58:33 +0200

    Update

--- lua/config/plugins/scscratchpad.lua
+++ lua/config/plugins/scscratchpad.lua
@@ -1,13 +1,13 @@
 require("sc-scratchpad").setup {
   keymaps = {
-    toggle = "<Leader>cp",
-    send = "<C-e>",
+    toggle = "<F4>",
+    send = "<C-CR>",
   },
   border = "rounded",
   position = "50%",
   width = "50%",
   height = "50%",
   firstline = "// Scratchpad",
-  open_insertmode = true,
+  open_insertmode = false,
   close_on_execution = false,
 }
