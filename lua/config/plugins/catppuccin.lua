b11adf8279b2c29f1008fcab0e17c06df8064ea4
Dieter Hoofman <dieter.hoofman@gmail.com>
2022-05-27 20:24:08 +0200

    Update

--- lua/config/plugins/catppuccin.lua
+++ lua/config/plugins/catppuccin.lua
@@ -1,19 +1,35 @@
-vim.g.catppuccin_flavour = "mocha"
+-- vim.g.catppuccin_flavour = "mocha"
 
--- dirty patch for old catppuccin colors
+-- restore the old catppuccin colors
 require("catppuccin.core.palettes.init").get_palette = function()
-  local mocha = require "catppuccin.core.palettes.mocha"
-  mocha.mauve = "#DDB6F2"
-  mocha.red = "#F28FAD"
-  mocha.maroon = "#E8A2AF"
-  mocha.peach = "#F8BD96"
-  mocha.yellow = "#FAE3B0"
-  mocha.green = "#ABE9B3"
-  mocha.teal = "#B5E8E0"
-  mocha.sky = "#89DCEB"
-  mocha.blue = "#96CDFB"
-  mocha.lavender = "#C9CBFF"
-  return mocha
+  return {
+    rosewater = "#F5E0DC",
+    flamingo = "#F2CDCD",
+    pink = "#F5C2E7",
+    mauve = "#DDB6F2",
+    red = "#F28FAD",
+    maroon = "#E8A2AF",
+    peach = "#F8BD96",
+    yellow = "#FAE3B0",
+    green = "#ABE9B3",
+    teal = "#B5E8E0",
+    sky = "#89DCEB",
+    sapphire = "#74C7EC",
+    blue = "#96CDFB",
+    lavender = "#C9CBFF",
+    text = "#C6D0F5",
+    subtext1 = "#B3BCDF",
+    subtext0 = "#A1A8C9",
+    overlay2 = "#8E95B3",
+    overlay1 = "#7B819D",
+    overlay0 = "#696D86",
+    surface2 = "#565970",
+    surface1 = "#43465A",
+    surface0 = "#313244",
+    base = "#1E1E2E",
+    mantle = "#181825",
+    crust = "#11111B",
+  }
 end
 
 require("catppuccin").setup {
@@ -94,6 +110,7 @@ require("catppuccin").remap {
   TSKeyword = { fg = c.red, style = "NONE" },
   TSField = { fg = c.lavender, style = "NONE" },
   TSComment = { fg = c.overlay0, style = "italic" },
+
   -- alpha
   AlphaButton = { fg = c.blue },
   AlphaButtonShortcut = { fg = c.peach },
