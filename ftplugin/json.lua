65c6fc6f6f984dcddb923a43e2d46eefd60f9bff
Dieter Hoofman <dieter.hoofman@gmail.com>
2022-06-10 22:33:05 +0200

    Update

--- ftplugin/json.lua
+++ ftplugin/json.lua
@@ -1,4 +1,4 @@
-local cmd = [[<Cmd>term echo]]
+local cmd = [[<Cmd>tabe %<Bar>term echo]]
   .. (vim.fn.has "win32" == 1 and [[.]] or [[ ""]])
   .. [[ | fzf --phony --print-query --preview-window="up:90\%" --preview "jq ]]
   .. (vim.fn.has "win32" == 1 and "" or [[--color-output ]])
