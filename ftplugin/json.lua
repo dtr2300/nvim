e1dd3b9e8d8ee331108d524b7b30f7f9dd2173b5
Dieter Hoofman <dieter.hoofman@gmail.com>
2022-06-12 15:12:16 +0200

    Update

--- ftplugin/json.lua
+++ ftplugin/json.lua
@@ -2,7 +2,7 @@ local cmd = [[<Cmd>tabe %<Bar>term echo]]
   .. (vim.fn.has "win32" == 1 and [[.]] or [[ ""]])
   .. [[ | fzf --phony --print-query --preview-window="up:90\%" --preview "jq ]]
   .. (vim.fn.has "win32" == 1 and "" or [[--color-output ]])
-  .. [[{q} \"%\""<CR>i]]
+  .. [[{q} \"%\""<CR><Cmd>setlocal nonu nornu<CR>i]]
 
 -- live jq preview
 vim.keymap.set("n", "<F2>", cmd, { silent = true, buffer = true })
--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,2 +1,2 @@
 -- preview markdown
-vim.keymap.set("n", "<F2>", '<Cmd>tabe %<Bar>term glow -p "%"<CR>i', { silent = true, buffer = true })
+vim.keymap.set("n", "<F2>", '<Cmd>tabe %<Bar>term glow -p "%"<CR><Cmd>setlocal nonu nornu<CR>i', { silent = true, buffer = true })
