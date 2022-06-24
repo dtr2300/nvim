4138c4bfc0dd9010f2fa9bd758700fedb6c4e4a3
Dieter Hoofman <dieter.hoofman@gmail.com>
2022-06-24 20:36:47 +0200

    Update

--- ftplugin/haskell.lua
+++ ftplugin/haskell.lua
@@ -2,14 +2,18 @@ if vim.fn.has "win32" == 0 then
   return
 end
 
+-- send current paragraph
 vim.keymap.set({ "n", "i" }, "<C-e>", function()
   require("config.utils.tidal").send(1)
 end, { silent = true, buffer = true })
 
+-- send current line
+vim.keymap.set({ "n", "i" }, "<M-e>", "<Cmd>ToggleTermSendCurrentLine 1<CR>", { silent = true, buffer = true })
+
 vim.keymap.set(
   "n",
   "<F5>",
-  [[<Cmd>1TermExec cmd="tidal.bat" size=10 direction=horizontal<CR>]],
+  [[<Cmd>1TermExec cmd="tidal.bat" size=7 direction=horizontal<CR>]],
   { silent = true, buffer = true }
 )
 
--- lua/config/utils/tidal.lua
+++ lua/config/utils/tidal.lua
@@ -1,9 +1,5 @@
 M = {}
 
-local api = vim.api
-local fn = vim.fn
-local exec = require("toggleterm").exec
-
 function M.send(terminal_id)
   terminal_id = terminal_id or 1
   terminal_id = tonumber(terminal_id)
@@ -12,45 +8,41 @@ function M.send(terminal_id)
     terminal_id = { terminal_id, "number", true },
   }
 
-  local current_window = api.nvim_get_current_win()
-  local b_line, b_col = unpack(api.nvim_win_get_cursor(0))
+  local b_line, _ = unpack(vim.api.nvim_win_get_cursor(0))
 
+  -- find paragraph start
   local startl = b_line
-  for i = b_line, 1, -1 do
-    if fn.getline(i) == "" then
-      break
-    end
-    startl = i
+  while vim.fn.getline(startl - 1) ~= "" do
+    startl = startl - 1
   end
 
+  -- find paragraph end
   local endl = b_line
-  while fn.getline(endl + 1) ~= "" do
+  while vim.fn.getline(endl + 1) ~= "" do
     endl = endl + 1
   end
 
-  local lines = fn.getline(startl, endl)
+  local lines = vim.fn.getline(startl, endl)
 
   if #lines == 0 or lines == nil then
     return
   end
 
+  -- strip whitespace
   for i, v in ipairs(lines) do
     lines[i] = v:gsub("^%s+", ""):gsub("%s+$", "")
   end
 
+  -- strip lines with only comments
   lines = vim.tbl_filter(function(value)
-    if value:find "^%-%-" ~= nil then
-      return false
-    end
-    return true
+    return value:find "^%-%-" == nil
   end, lines)
+
+  -- TODO: strip inline comments
 
   local v = table.concat(lines, " ")
 
-  exec(v, terminal_id)
-
-  api.nvim_set_current_win(current_window)
-  api.nvim_win_set_cursor(current_window, { b_line, b_col })
+  require("toggleterm").exec(v, terminal_id, nil, nil, nil, nil, false)
 end
 
 return M
