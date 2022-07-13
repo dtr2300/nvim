19da38d018da03793800da0284919d0f9c5d2d44
Dieter Hoofman <dieter.hoofman@gmail.com>
2022-07-13 13:26:07 +0200

    Update

--- lua/config/utils/tidal.lua
+++ lua/config/utils/tidal.lua
@@ -2,6 +2,10 @@ M = {}
 
 local exec = require("toggleterm").exec
 
+local function getline(row)
+  return vim.fn.getline(row):gsub("^%s+", "")
+end
+
 function M.send(terminal_id, send_paragraph)
   vim.validate {
     terminal_id = { terminal_id, "number", true },
@@ -11,24 +15,25 @@ function M.send(terminal_id, send_paragraph)
   send_paragraph = send_paragraph == nil or send_paragraph
 
   local startl, _ = unpack(vim.api.nvim_win_get_cursor(0))
-  local lines = { vim.fn.getline(startl) }
-  if lines[1] == "" then
+  local lines = getline(startl)
+  if lines == "" then
     return
   end
+  lines = { lines }
   local endl = startl
 
   -- find start and end of paragraph
   if send_paragraph then
     local line
     repeat
-      line = vim.fn.getline(startl - 1)
+      line = getline(startl - 1)
       if line ~= "" then
         table.insert(lines, 1, line)
         startl = startl - 1
       end
     until line == ""
     repeat
-      line = vim.fn.getline(endl + 1)
+      line = getline(endl + 1)
       if line ~= "" then
         table.insert(lines, line)
         endl = endl + 1
@@ -38,7 +43,7 @@ function M.send(terminal_id, send_paragraph)
 
   -- strip whitespace and comments
   lines = vim.tbl_map(function(line)
-    return line:gsub("^%s+", ""):gsub("%-%-.+$", ""):gsub("%s+$", "")
+    return line:gsub("%-%-.+$", ""):gsub("%s+$", "")
   end, lines)
 
   -- remove empty lines
@@ -55,7 +60,7 @@ function M.send(terminal_id, send_paragraph)
 
   -- flash
   local ns = vim.api.nvim_create_namespace "tidal_flash"
-  vim.highlight.range(0, ns, "SCNvimEval", { startl - 1, 0 }, { endl, 100000 }, { inclusive = true })
+  vim.highlight.range(0, ns, "SCNvimEval", { startl - 1, 0 }, { endl - 1, 100000 }, { inclusive = true })
   vim.defer_fn(function()
     vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
   end, 200)
