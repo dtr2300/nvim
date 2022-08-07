0a89f2e32e04a67e9b94b74ae20b78f754abf4fc
Dieter Hoofman <dieter.hoofman@gmail.com>
2022-08-07 17:17:47 +0200

    Update

--- lua/config/plugins/alpha.lua
+++ lua/config/plugins/alpha.lua
@@ -1,20 +1,51 @@
 math.randomseed(os.time())
 
---- @param args table
-local function button(args)
-  local b = require("alpha.themes.dashboard").button(args[1], args[2], args.keybind, args.keybind_opts)
-  b.opts.hl = args.hl or "AlphaButton"
-  b.opts.hl_shortcut = args.hl_shortcut or "AlphaButtonShortcut"
-  b.opts.position = args.position or "center"
-  b.opts.width = args.width or 35
+--- @param sc string
+--- @param txt string
+--- @param keybind string?
+--- @param keybind_opts table?
+--- @return table
+local function button(sc, txt, keybind, keybind_opts)
+  local b = require("alpha.themes.dashboard").button(sc, txt, keybind, keybind_opts)
+  b.opts.hl = "AlphaButton"
+  b.opts.hl_shortcut = "AlphaButtonShortcut"
+  b.opts.width = 35
   return b
 end
 
---- @param value number?
-local function pad(value)
-  return { type = "padding", val = value or 2 }
+--- @param value table|string
+--- @param hl string?
+--- @return table
+local function text(value, hl)
+  return {
+    type = "text",
+    val = value,
+    opts = {
+      position = "center",
+      hl = hl or "Normal",
+    },
+  }
 end
 
+--- @param value number
+--- @return table
+local function pad(value)
+  return { type = "padding", val = value }
+end
+
+--- @param items table
+--- @param spacing number?
+local function group(items, spacing)
+  return {
+    type = "group",
+    val = items,
+    opts = {
+      spacing = spacing or 0,
+    },
+  }
+end
+
+--- @return string
 local function info()
   local plugins = #vim.tbl_keys(packer_plugins)
   local v = vim.version()
@@ -23,71 +54,34 @@ local function info()
   return string.format(" %d   v%d.%d.%d %s  %s", plugins, v.major, v.minor, v.patch, platform, datetime)
 end
 
--- header
-local header = {
-  type = "text",
-  val = require("config.utils.headers").random(),
-  opts = {
-    position = "center",
-    hl = "AlphaCol" .. math.random(5),
-  },
-}
-
--- subheader
-local subheader = {
-  type = "text",
-  val = info(),
-  opts = {
-    position = "center",
-    hl = header.opts.hl,
-  },
-}
-
--- buttons
-local buttons = {
-  type = "group",
-  val = {
-    button { "SPC t o", "  Recently opened files" },
-    button { "SPC t f", "  Find file" },
-    button { "SPC t l", "  Find word" },
-    button { "SPC t F", "  File browser" },
-    button { "SPC t 1", "  Find repo" },
-    button { "SPC t s", "  Open session" },
-    button { "SPC c n", "  New file" },
-    button { "SPC p u", "  Update plugins" },
-    button { "q", "  Quit", keybind = "<Cmd>qa<CR>" },
-  },
-  opts = {
-    spacing = 0,
-  },
-}
-
--- quote
-local quote = {
-  type = "text",
-  val = require "alpha.fortune"(),
-  opts = {
-    position = "center",
-    hl = "AlphaQuote",
-  },
-}
+local header_color = "AlphaCol" .. math.random(5)
 
 require("alpha").setup {
   layout = {
-    pad(),
-    header,
+    pad(2),
+    text(require("config.utils.headers").random(), header_color),
     pad(1),
-    subheader,
-    pad(),
-    buttons,
+    text(info(), header_color),
+    pad(2),
+    group {
+      button("SPC t o", "  Recently opened files"),
+      button("SPC t f", "  Find file"),
+      button("SPC t l", "  Find word"),
+      button("SPC t F", "  File browser"),
+      button("SPC t 1", "  Find repo"),
+      button("SPC t s", "  Open session"),
+      button("SPC c n", "  New file"),
+      button("SPC p u", "  Update plugins"),
+      button("q", "  Quit", "<Cmd>qa<CR>"),
+    },
     pad(1),
-    quote,
+    text(require "alpha.fortune"(), "AlphaQuote"),
   },
   opts = {
     setup = function()
       vim.api.nvim_create_autocmd("User", {
         pattern = "AlphaReady",
-        desc = "disable status and tabline for alpha",
+        desc = "Disable status and tabline for alpha",
         callback = function()
           vim.go.laststatus = 0
           vim.opt.showtabline = 0
@@ -96,7 +90,7 @@ require("alpha").setup {
       })
       vim.api.nvim_create_autocmd("BufUnload", {
         buffer = 0,
-        desc = "enable status and tabline after alpha",
+        desc = "Enable status and tabline after alpha",
         callback = function()
           vim.go.laststatus = 3
           vim.opt.showtabline = 2
